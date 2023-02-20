3cd9c253366dbd1421efd09c649f31cec61b4076
Axel Brisse <axel.brisse@gmail.com>
2023-02-20 17:20:06 +0100

    GOOD

--- parse_equation.py
+++ parse_equation.py
@@ -2,13 +2,12 @@ from collections import defaultdict
 from dataclasses import dataclass
 from decimal import Decimal
 import re
-
 from utils import ft_assert
 
 LEGIT_CHARACTERS = "0123456789X=.-+*^"
-REGEX_A = r"(?:[+-])(?:\d+)(?:\.\d+)?"
+REGEX_A = r"(?:\d+)(?:\.\d+)?"
 REGEX_P = r"X(?:\^\d+)?"
-REGEX_MONOMIAL = rf"({REGEX_A})\*({REGEX_P})|({REGEX_A})|({REGEX_P})"
+REGEX_MONOMIAL = rf"([+-])(?:({REGEX_A})\*({REGEX_P})|({REGEX_A})|({REGEX_P}))"
 REGEX_POLYNOMIAL = rf"({REGEX_MONOMIAL})+"
 
 
@@ -18,8 +17,9 @@ class Monomial:
     p: int
 
 
-def create_monomial(a1, p1, a2, p2):
-    a = Decimal(1 if p2 else a1 if a1 else a2)
+def create_monomial(sign, a1, p1, a2, p2):
+    sign = 1 if sign == "+" else -1
+    a = sign * Decimal(1 if p2 else a1 if a1 else a2)
     p = p1 or p2
     p = 0 if p == "" else 1 if p == "X" else int(p[2:])
     return Monomial(a, p)
@@ -35,7 +35,7 @@ def reduce_equation(left, right):
 
 
 def parse_polynomial(s):
-    if s[0].isdigit():
+    if s[0] not in "-+":
         s = "+" + s
     ft_assert(re.fullmatch(REGEX_POLYNOMIAL, s), f'"{s}" is not properly formatted')
     polynomial = re.findall(REGEX_MONOMIAL, s)
@@ -49,6 +49,7 @@ def parse_equation(s):
         all(c in LEGIT_CHARACTERS for c in s), "Unknown character present in string"
     )
     left, right = s.split("=")
-    assert left and right
+    ft_assert(left, f"{left} is empty")
+    ft_assert(right, f"{right} is empty")
     left, right = parse_polynomial(left), parse_polynomial(right)
     return reduce_equation(left, right)
--- test_computor.py
+++ test_computor.py
@@ -4,18 +4,27 @@ import pytest
 
 
 def check_equation(s, expected):
-    assert parse_equation(s) == expected
+    assert parse_equation(s) == {p: Decimal(a) for p, a in expected.items()}
 
 
 def test_parse_equation_good():
     check_equation("0=0", {})
-    check_equation("1=0", {0: Decimal("1")})
-    check_equation("X=0", {1: Decimal("1")})
-    check_equation("X^2=0", {2: Decimal("1")})
-    check_equation("1*X^2=0", {2: Decimal("1")})
+    check_equation("1=0", {0: "1"})
+    check_equation("X=0", {1: "1"})
+    check_equation("X^2=0", {2: "1"})
+    check_equation("1*X^2=0", {2: "1"})
+    check_equation(
+        "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0", {0: "4", 1: "4", 2: "-9.3"}
+    )
+    check_equation("5 * X^0 + 4 * X^1 = 4 * X^0", {0: "1", 1: "4"})
+    check_equation(
+        "8 * X^0 - 6 * X^1 + 0 * X^2 - 5.6 * X^3 = 3 * X^0",
+        {0: "5", 1: "-6", 3: "-5.6"},
+    )
+    check_equation("5 + 4 * X + X^2= X^2", {0: "5", 1: "4"})
 
 
 def test_parse_equation_bad():
-    for s in ["42", "x = 0", "2X = 0"]:
+    for s in ["42", "x = 0", "2X = 0", "="]:
         with pytest.raises(SystemExit):
             parse_equation(s)
