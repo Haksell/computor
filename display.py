5878b07330f1da00e970613443191ffd7ea0c408
Axel Brisse <axel.brisse@gmail.com>
2023-02-20 18:54:51 +0100

    cardano

--- display.py
+++ display.py
@@ -1,4 +1,3 @@
-from math import sqrt
 from utils import get_degree
 
 
@@ -29,21 +28,22 @@ def display_complex(z):
     if real.is_integer() and imag.is_integer():
         real = int(real)
         imag = int(imag)
-    if real and imag:
-        print(f"{real}{imag:+}i")
-    elif real:
-        print(f"{real}")
-    elif imag:
-        print(f"{imag}i")
-    else:
-        print("0")
+    print(
+        f"{real}{imag:+}i"
+        if real and imag
+        else f"{real}"
+        if real
+        else f"{imag}i"
+        if imag
+        else "0"
+    )
 
 
-def display_second_degree(a, b, c):
+def display_second_degree(reduced):
+    a, b, c = (reduced.get(2, 0), reduced.get(1, 0), reduced.get(0, 0))
     discriminant = b**2 - 4 * a * c
-    a2 = 2 * a
-    x1 = (-b - discriminant**0.5) / a2
-    x2 = (-b + discriminant**0.5) / a2
+    x1 = (-b - discriminant**0.5) / (2 * a)
+    x2 = (-b + discriminant**0.5) / (2 * a)
     if discriminant == 0:
         print("Discriminant zero, the solution of multiplicity 2 is:")
         display_complex(x1)
@@ -54,21 +54,50 @@ def display_second_degree(a, b, c):
         display_complex(x2)
 
 
+# https://stackoverflow.com/a/74198367/10793260
+def display_third_degree(reduced):
+    def gen_cbrt(polynomial):
+        solution = set()
+        root1 = polynomial ** (1 / 3)
+        root2 = (polynomial ** (1 / 3)) * (-1 / 2 + (3**0.5 * 1j) / 2)
+        root3 = (polynomial ** (1 / 3)) * (-1 / 2 - (3**0.5 * 1j) / 2)
+        solution.update({root1, root2, root3})
+        return solution
+
+    def cardano(a, b, c, d):
+        solutions = set()
+        p = (3 * a * c - b**2) / (3 * a**2)
+        q = (2 * b**3 - 9 * a * b * c + 27 * a**2 * d) / (27 * a**3)
+        alpha = gen_cbrt(-q / 2 + ((q / 2) ** 2 + (p / 3) ** 3) ** 0.5)
+        beta = gen_cbrt(-q / 2 - ((q / 2) ** 2 + (p / 3) ** 3) ** 0.5)
+        for i in alpha:
+            for j in beta:
+                if abs((i * j) + p / 3) <= 0.00001:
+                    z = i + j - b / (3 * a)
+                    solutions.add(
+                        complex(round(z.real, PRECISION), round(z.imag, PRECISION))
+                    )
+        return solutions
+
+    solutions = cardano(*[float(reduced.get(i, 0)) for i in [3, 2, 1, 0]])
+    print(f"There are {len(solutions)} solutions:")
+    for solution in solutions:
+        display_complex(solution)
+
+
 def display_solutions(reduced):
-    a, b, c = (
-        float(reduced.get(2, 0)),
-        float(reduced.get(1, 0)),
-        float(reduced.get(0, 0)),
-    )
     degree = get_degree(reduced)
+    reduced = {p: float(a) for p, a in reduced.items()}
     if degree == -1:
         print("There are infinitely many solutions.")
     elif degree == 0:
         print("There are no solutions.")
     elif degree == 1:
         print("The solution is:")
-        display_complex(-c / b)
+        display_complex(-reduced.get(0, 0) / reduced.get(1, 0))
     elif degree == 2:
-        display_second_degree(a, b, c)
+        display_second_degree(reduced)
+    elif degree == 3:
+        display_third_degree(reduced)
     else:
         print("The polynomial degree is strictly greater than 2, I can't solve.")
